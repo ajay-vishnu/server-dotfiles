b59675750843f1b8199b62fd1013b8169d3892fb
Ajay Vishnu V <ajayvishnu950@gmail.com>
2025-06-25 13:24:42 +0530

    :sparkles: added new keybinds for dap

--- nvim/.config/nvim/lua/core/plugins/dap.lua
+++ nvim/.config/nvim/lua/core/plugins/dap.lua
@@ -11,21 +11,55 @@ return {
 			local dap = require("dap")
 			local dapui = require("dapui")
 
+			require("nvim-dap-virtual-text").setup({
+				commented = true, -- Show virtual text alongside comment
+			})
+
+			vim.fn.sign_define("DapBreakPoint", {
+				text = "",
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
+
+			vim.fn.sign_define("DapBreakPointRejected", {
+				text = "",
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
+
+			vim.fn.sign_define("DapStopped", {
+				text = "",
+				texthl = "DiagnosticSignWarn",
+				linehl = "",
+				numhl = "DiagnosticSignWarn",
+			})
+
 			dapui.setup()
+			-- Automatically open/close DAP UI
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
 			end
+
 			dap.listeners.before.event_terminated["dapui_config"] = function()
 				dapui.close()
 			end
+
 			dap.listeners.before.event_exited["dapui_config"] = function()
 				dapui.close()
 			end
-			require("nvim-dap-virtual-text").setup()
 
-			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<leader>dc", dap.continue)
-			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor)
+			local opts = { noremap = true, silent = true }
+
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, opts)
+			vim.keymap.set("n", "<leader>dc", dap.continue, opts)
+			vim.keymap.set("n", "<leader>do", dap.step_over, opts)
+			vim.keymap.set("n", "<leader>di", dap.step_into, opts)
+			vim.keymap.set("n", "<leader>dO", dap.step_out, opts)
+			vim.keymap.set("n", "<leader>dq", dap.terminate, opts)
+			vim.keymap.set("n", "<leader>du", dapui.toggle, opts)
+			vim.keymap.set("n", "<leader>gb", dap.run_to_cursor, opts)
 		end,
 	},
 }
