6dacf63d53fc6e1872866cb9cf0d0d1476522886
Ajay Vishnu V <ajayvishnu950@gmail.com>
2025-07-06 20:24:22 +0530

    :ambulance: fix(neovim): makeshift fix to attach linter when opened a file for the first time in nvim
    credit to https://github.com/ojas-bhagavath

--- nvim/.config/nvim/lua/core/config/autocommands.lua
+++ nvim/.config/nvim/lua/core/config/autocommands.lua
@@ -117,3 +117,12 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 		vim.diagnostic.open_float(nil, { focus = false })
 	end,
 })
+
+-- set filetype automatically (temporary fix)
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function(args)
+		if vim.bo[args.buf].filetype == "" then
+			vim.cmd("filetype detect")
+		end
+	end,
+})
